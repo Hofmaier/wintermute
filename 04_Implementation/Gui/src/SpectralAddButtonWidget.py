fcf704f14d7f838a4d19d14675043789729c0e51
Raphi Kohler <rkohler@hsr.ch>
2012-10-20 16:04:58 +0200

    minor change

--- 04_Implementation/Gui/src/SpectralAddButtonWidget.py
+++ 04_Implementation/Gui/src/SpectralAddButtonWidget.py
@@ -14,9 +14,10 @@ class SpectralAddButtonWidget(QtGui.QWidget):
 	
 	self.spectralAddButton = QtGui.QPushButton()
 	self.spectralAddButton.setText("+")
-	self.spectralAddButtonLayout.setColumnMinimumWidth(0, 570)
 	self.spectralAddButtonLayout.setColumnMinimumWidth(2, 4)
+	spacerItem = QtGui.QSpacerItem(0, 0, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
 	self.spectralAddButtonLayout.addWidget(self.spectralAddButton, 0, 1)
+	self.spectralAddButtonLayout.addItem(spacerItem, 0, 0)
 	
 	QtCore.QObject.connect(self.spectralAddButton, QtCore.SIGNAL(_fromUtf8("clicked()")), mainGui.addSpectralColourWidget)
 
